683f792bc77a5cb1ee1f58848fd07e3abf383825
ChiakiTakayama <106967678+ChiakiTakayama@users.noreply.github.com>
2022-06-26 12:21:01 +0000

    [update]投稿後詳細画面にリンクするまで

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,12 +4,13 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def create#投稿の保存機能。今は/topに飛ぶようにしているが詳細画面に飛ばすようにする
     book = Book.new(book_params)
     book.save
-    redirect_to '/top'
+    redirect_to book_path(book.id)
   end
 
   def show #詳細画面。投稿押したら飛んでくる画面
@@ -17,6 +18,7 @@ class BooksController < ApplicationController
   end
 
   def edit
+    # @book = Book.find(params[:id])  showが正常に動いてない状態
   end
   # ここから先は保存機能と同時期に書いてる
   private
