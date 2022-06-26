6ee3fa88583ae879fd7aaa4258b88942ae79c5ae
ChiakiTakayama <106967678+ChiakiTakayama@users.noreply.github.com>
2022-06-26 01:47:41 +0000

    [update]投稿保存機能追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,9 +6,20 @@ class BooksController < ApplicationController
     @book = Book.new
   end
 
+  def create#投稿の保存機能。今は/topに飛ぶようにしている
+    book = Book.new(book_params)
+    book.save
+    redirect_to '/top'
+  end
+
   def show #詳細画面。投稿押したら飛んでくる画面
   end
 
   def edit
+  end
+  # ここから先は保存機能と同時期に書いてる
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
