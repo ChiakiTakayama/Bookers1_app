befe1ed569b52efad479aeb3df2dff5179b0ff66
ChiakiTakayama <106967678+ChiakiTakayama@users.noreply.github.com>
2022-06-28 11:52:04 +0000

    [update]バリデーション関係解決？

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,15 +8,16 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
-    # @book = Book.new(book_params)
-    # if @book.save
-      # redirect_to book_path(@book.id)
-    # else
-      # render :index
-    # end  
+    # book = Book.new(book_params)
+    # book.save
+    # redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index
+    end  
   end
 
   def show #詳細画面。投稿押したら飛んでくる画面
